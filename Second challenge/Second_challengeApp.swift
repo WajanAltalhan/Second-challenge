74c7398fd6729d06999b1d38fbd05709de0b822b
Wajan <wajanaltalhan@gmail.com>
2024-10-20 09:36:27 +0300

    Initial Commit

---
+++ Second challenge/Second_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Second_challengeApp.swift
+//  Second challenge
+//
+//  Created by Wajan Altalhan on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Second_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Second challengeTests/Second_challengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Second_challengeTests.swift
+//  Second challengeTests
+//
+//  Created by Wajan Altalhan on 17/04/1446 AH.
+//
+
+import Testing
+@testable import Second_challenge
+
+struct Second_challengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
